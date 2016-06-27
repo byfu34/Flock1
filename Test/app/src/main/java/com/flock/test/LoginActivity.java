a370b071300d42a37cbeda2fe5af23e63fa6c8b4
byfu34 <bmbg439@yahoo.com>
2016-06-27 15:51:07 -0500

    A lot of stuff

--- Test/app/src/main/java/com/flock/test/LoginActivity.java
+++ Test/app/src/main/java/com/flock/test/LoginActivity.java
@@ -2,6 +2,7 @@ package com.flock.test;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import com.flock.android.flock.R;
  */
 public class LoginActivity extends Activity {
     private Button facebookButton;
+    public static final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     public void onCreate( Bundle savedInstanceState )
@@ -55,6 +57,11 @@ public class LoginActivity extends Activity {
                 super.handleResponse( isValidLogin );
             }
         });
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean hasLoggedIn = settings.getBoolean("loggedIn", false);
+        
+
     }
 
     private void initUI()
